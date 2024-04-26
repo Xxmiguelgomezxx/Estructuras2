a2629114bd8edede434e51233c9f80193fd37642
Miguel Ángel Gómez García <miguel07050466@gmail.com>
2024-04-25 19:49:31 -0500

    proyecto

--- proyecto.java
+++ proyecto.java
@@ -1,5 +1,19 @@
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class proyecto{
     public static void main(String[] args) {
-        
+        LinkedList lista = new LinkedList<>();
+        LinkedList<empleado>empleados = new LinkedList<>();
+        Scanner sc = new Scanner(System.in );
+for (int i = 0;  i<3; i++) {
+    empleado item = new empleado();
+    System.out.println();
+                
+}
+      }
+
+     
     }
 }
